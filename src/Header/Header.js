c4eb5080045fcac41fa7aec4d8a4ffc466bae886
Rincy Pradeep <rincypradeep15@gmail.com>
2021-08-17 19:34:07 +0530

    completed navbar and banner

---
+++ src/Header/Header.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket'
+
+const Header = () => {
+    return (
+        <nav className="header">
+            {/* logo */}
+            <Link to="/">
+                <img className="header-logo" src='/images/amazon_logo.png' alt="logo" />
+            </Link>
+
+            {/* input field */}
+            <div className="header-search">
+                <input type="text" className="header-searchInput" />
+                <SearchIcon className="header-searchIcon" />
+            </div>
+
+            {/* nav options */}
+            <div className="header-nav">
+                <div className="header-link">
+                    <Link to="/login" className="header-link">
+                        <div className="header-option">
+                            <span className="header-option-one">Hello Rincy</span>
+                            <span className="header-option-two">Sign In</span>
+                        </div>
+                    </Link>
+
+                    <Link to="/" className="header-link">
+                        <div className="header-option">
+                            <span className="header-option-one">Returns</span>
+                            <span className="header-option-two">& Orders</span>
+                        </div>
+                    </Link>
+
+                    <Link to="/" className="header-link">
+                        <div className="header-option">
+                            <span className="header-option-one">Your</span>
+                            <span className="header-option-two">Prime</span>
+                        </div>
+                    </Link>
+
+                    <Link to="/checkout" className="header-link">
+                        <div className="header-option-cart">
+                            <ShoppingBasket />
+                            <span className="header-option-two cart-count">0</span>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <img src="/images/banner.jpg" alt="banner" className="banner" />
+        </div>
+    )
+}
+
+export default Home
